87ba1fc09f41169f145da6a05868b9acb9e3ca04
RainLiu19980309 <2584307116@qq.com>
2022-03-31 13:12:21 -0400

    done

--- Roku_Flashback/api/index.php
+++ Roku_Flashback/api/index.php
@@ -1,19 +1,26 @@
 <?php
   // route requests for CRUD
   require('../config/connect.php');
+// this needs to come back out once the post is working
+// this is demo only
+
+  if (isset($_POST['username'])) {
+      echo 'username via POST:' . $_POST["username"];
+      die('  ...and imma stop here');
+  }
 
   $query = "SELECT * FROM tbl_movies";
 
   if (isset($_GET['id'])) {
-    $movID = $_GET['id'];
-    $query .= ' WHERE movies_id="'.$movID.'"';
+      $movID = $_GET['id'];
+      $query .= ' WHERE movies_id="'.$movID.'"';
   }
 
   $result = array();
   $runQuery = $pdo->query($query);
 
-  while($row = $runQuery->fetchAll(PDO::FETCH_ASSOC)) {
-    $result[] = $row;
+  while ($row = $runQuery->fetchAll(PDO::FETCH_ASSOC)) {
+      $result[] = $row;
   }
 
   echo json_encode($result);
